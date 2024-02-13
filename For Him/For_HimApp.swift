c635b9fc180e9038da867a4e8bfa0c5b08224a4f
Ljutica Nojinovic <ljutica@despotsoft.com>
2024-02-13 11:38:20 +0100

    Initial Commit

---
+++ For Him/For_HimApp.swift
@@ -0,0 +1,17 @@
+//
+//  For_HimApp.swift
+//  For Him
+//
+//  Created by Stefan Gusatovic on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct For_HimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
